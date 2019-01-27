3f1d6d9f73df69b4e5053a1676c15deedd9ca8f5
NobbsNobby <art.kotov1@gmail.com>
2019-01-27 15:26:54 +0300

    2

--- source/basic-redux/init/store.js
+++ source/basic-redux/init/store.js
@@ -1,7 +1,32 @@
 //Core
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
 
 //Reducer
 import { rootReducer } from './rootReducer';
 
-export const store = createStore(rootReducer);
+const logger = createLogger({
+    duration:  true,
+    collapsed: true,
+    colors:    {
+        title:     () => '#139BFE',
+        prevState: () => '#1C5FAF',
+        action:    () => '#149945',
+        nextState: () => '#A47104',
+        error:     () => '#FF0005',
+    },
+});
+
+const preloadedState = JSON.parse(localStorage.getItem('gallery'));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const enhancedStore = composeEnhancers(applyMiddleware(logger));
+
+export const store = preloadedState
+    ? createStore(rootReducer, preloadedState, enhancedStore)
+    : createStore(rootReducer, enhancedStore);
+
+store.subscribe(() => {
+    const state = store.getState();
+
+    localStorage.setItem('gallery', JSON.stringify(state));
+});
