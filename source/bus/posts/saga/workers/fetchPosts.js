4e82fccc01aeba61bcdf7675f853a5e7d156c6a7
Artem <art.kotov1@gmail.com>
2019-02-03 20:34:44 +0300

    done v1.0

--- source/bus/posts/saga/workers/fetchPosts.js
+++ source/bus/posts/saga/workers/fetchPosts.js
@@ -6,19 +6,19 @@ import { api } from '../../../../REST';
 import { postsActions } from '../../actions';
 import { uiActions } from '../../../ui/actions';
 
-export function* createPost ({ payload: comment }) {
+export function* fetchPosts () {
     try {
         yield put(uiActions.startFetching());
-        const response = yield apply(api, api.posts.create, [comment]);
-        const { data: post, message } = yield apply(response, response.json);
+        const response = yield apply(api, api.posts.fetch);
+        const { data: posts, message } = yield apply(response, response.json);
 
         if (response.status !== 200) {
             throw new Error(message);
         }
 
-        yield put(postsActions.createPost(post));
+        yield put(postsActions.fillPosts(posts));
     } catch (error) {
-        yield put(uiActions.emitError(error, 'createPost worker'));
+        yield put(uiActions.emitError(error, 'fetchPosts worker'));
     } finally {
         yield put(uiActions.stopFetching());
     }
