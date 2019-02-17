90790dbac94ca48e0fa62d360ec051ea9a7f80f0
Artem <art.kotov1@gmail.com>
2019-02-17 19:15:57 +0300

    homework 8

--- source/navigation/Private.js
+++ source/navigation/Private.js
@@ -21,6 +21,7 @@ export default class Private extends Component {
 
     componentWillUnmount () {
         socket.removeListener('create');
+        socket.removeListener('remove');
     }
 
     render () {
